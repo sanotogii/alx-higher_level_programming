a18f3077723d5b3f65fee3fab244d757a2d775f5
sanotogii <nasaereh@gmail.com>
2023-08-08 21:08:07 +0000

    pycode

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for harf in range(ord('a'), ord('z')+1):
     if harf != ord('e') and harf != ord('q'):
-    	print("{}".format(chr(harf)), end='')
+        print("{}".format(chr(harf)), end='')
