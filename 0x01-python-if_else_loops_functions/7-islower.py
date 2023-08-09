1973432cecabcafe6c9c21a5f2f0589e36e0124e
sanotogii <nasaereh@gmail.com>
2023-08-09 13:57:02 +0000

    fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) <= ord('z') and int(c) >= ord('a'):
+    if ord(c) <= ord('z') and ord(c) >= ord('a'):
         return (True)
     else:
         return (False)
