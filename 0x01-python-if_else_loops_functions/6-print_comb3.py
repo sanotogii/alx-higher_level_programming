a770ab5a3198f77b22565c3f9c6ae4044d0272a8
sanotogii <nasaereh@gmail.com>
2023-08-09 15:39:41 +0000

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,7 @@
 for i in range(10):
     for j in range(i+1, 10):
         if i == 8:
-            sp = " "
+            print("{}{}".format(i, j), end=" ")
         else:
-            sp = ", "
-        print("{}{}".format(str(i), str(j)), end=sp)
+            print("{}{}".format(i, j), end=", ")
 print()
