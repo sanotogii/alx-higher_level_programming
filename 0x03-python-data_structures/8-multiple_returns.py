bfd6fb985df38aa71a38e043808844469abf9bce
sanotogii <nasaereh@gmail.com>
2023-08-13 13:39:21 -0700

    task 8+

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     lenn = len(sentence)
-    if sentence == []:
+    if sentence == "":
         t = (len, None)
     else:
         t = (lenn, sentence[0])
