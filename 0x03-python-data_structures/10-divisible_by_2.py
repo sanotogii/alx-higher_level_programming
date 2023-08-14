26e2ca335187ba396cd20a20d79cd5cc7085fc4e
sanotogii <nasaereh@gmail.com>
2023-08-14 03:04:44 -0700

    task 10+

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if len(my_list) == 0:
-        pass
+        return
     t = []
     for i in range(len(my_list)-1):
         if my_list[i] % 2 == 0:
