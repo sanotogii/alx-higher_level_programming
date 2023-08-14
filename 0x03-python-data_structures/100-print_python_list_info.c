ebb231a84474768fb5be6fd6cc64df083865e4db
sanotogii <nasaereh@gmail.com>
2023-08-14 04:15:30 -0700

    adv task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,30 @@
+#include <python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - print info of python list
+ * @p: pointer
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, all, i;
+	PyObject *element;
+	char str;
+
+	if (PyList_check(p))
+	{
+		size = PyList_size(p);
+		all = ((PyListObject *)p)->allocated;
+
+	        printf("[*] Size of the Python List = %ld\n", size);
+        	printf("[*] Allocated = %ld\n", alloc);
+
+		for (i = 0; i < size; i++)
+		{
+			element = PyList_GetItem(p, i);
+			str = Py_TYPE(item)->tp_name;
+			printf("Element %ld: %s\n", i, str);
+		}
+	}
+}
