67e88c2987dffcc7da105c337a208120adf2a89d
sanotogii <nasaereh@gmail.com>
2023-08-16 13:50:40 -0700

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_score = -float("inf")
+
+    for key, value in a_dictionary.items():
+        if value > best_score:
+            best = key
+            best_score = value
+
+    return best
