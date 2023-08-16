9b3f52e6304113323999024aec3f94a3df8c3142
sanotogii <nasaereh@gmail.com>
2023-08-16 10:47:35 -0700

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if not my_list:
+        pass
+    for i in my_list:
+        new_list = []
+        if i in new_list:
+            pass
+        else:
+            new_list.append(i)
+    somme = 0
+    for num in new_list:
+        somme += num
+    return num
