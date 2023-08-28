4170ffa521598421fe280000d2ca8e8d076f06b0
sanotogii <nasaereh@gmail.com>
2023-08-28 15:27:56 -0700

    adv task

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    ret = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                ret += (a ** b) /i
+        except:
+            ret = b + a
+            break
+    return ret
