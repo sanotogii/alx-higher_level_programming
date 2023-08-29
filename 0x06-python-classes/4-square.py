aedf925a9c5a23b446525803b34116f4573fe111
sanotogii <nasaereh@gmail.com>
2023-08-29 08:38:30 -0700

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""class square with size"""
+
+
+class Square:
+    """class square defined by it's size"""
+
+    def __init__(self, size=0):
+        """Size validation"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area of a square method"""
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """size"""
+
+        return self.__size
+
+    @setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
