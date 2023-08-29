6cf05b3c70a63a60b9036d19fbb7841dc65d229d
sanotogii <nasaereh@gmail.com>
2023-08-29 08:17:33 -0700

    task 2 update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,6 +8,6 @@ class Square:
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
-        eif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
