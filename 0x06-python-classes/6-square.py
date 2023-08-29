7be3f4bb4d2c250854b9a5d8b18e89e713a80127
sanotogii <nasaereh@gmail.com>
2023-08-29 10:44:22 -0700

    task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""class square with size"""
+
+
+class Square:
+    """class square defined by it's size"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Size validation"""
+
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Area of a square method"""
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if ((type(value) != tuple) or len(value) != 2) \
+        or (not all(isinstance(coord, int)) and coord >= 0 for coord in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
