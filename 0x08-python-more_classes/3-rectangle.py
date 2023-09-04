a45c1e834b956496eca684dfcebfe2b535b0a4a1
sanotogii <nasaereh@gmail.com>
2023-09-04 17:22:21 +0000

    task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""define a rectangle"""
+
+
+class Rectangle:
+    """class"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        _str = ""
+        if self.__height != 0 and self.__width != 0:
+            for i in range(0, self.__height):
+                _str += "#" * self.__width
+                _str += "\n"
+        return _str
+
+    @property
+    def width(self):
+        """width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """hieght"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
