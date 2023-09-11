28c22f74453a0adc470edfd5e052a5cb516e4dd7
sanotogii <nasaereh@gmail.com>
2023-09-11 17:09:13 -0400

    1

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class MyInt(int):
+    """class"""
+
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
