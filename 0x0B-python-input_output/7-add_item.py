bd4362dfa3f2dcd390932d39cca1e7038539db39
sanotogii <nasaereh@gmail.com>
2023-09-12 18:06:59 -0400

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ import sys
 import os
 
 
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 if os.path.exists("add_item.json"):
     items = load_from_json_file("add_item.json")
 else:
