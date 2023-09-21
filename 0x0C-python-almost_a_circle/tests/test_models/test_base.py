ab9f37d284c3731cd1e1a209bd34cc5f6ec5ab76
sanotogii <nasaereh@gmail.com>
2023-09-21 14:16:38 -0400

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -16,6 +16,14 @@ class TestBase(unittest.TestCase):
     def test_base(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
 
 if __name__ == '__main__':
     unittest.main()
