0a3e072922bb100d3f29b00b9d531fe079a814d0
sanotogii <nasaereh@gmail.com>
2023-09-22 16:41:08 -0400

    first task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Base class"""
+
+
+class Base:
+    """init"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """func"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
