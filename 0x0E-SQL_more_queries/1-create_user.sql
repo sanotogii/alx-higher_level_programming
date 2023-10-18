a4d28fa0d05b256214360f69b1d7994dc1aa4698
sanotogii <nasaereh@gmail.com>
2023-10-18 11:26:49 -0700

    task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- script that creates the MySQL server user user_0d_1.
+CREAT IF NOT EXIST 'username'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
