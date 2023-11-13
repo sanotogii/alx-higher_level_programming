77b188da91cc7da27f38b06cc5303fa8d66b3d3d
sanotogii <nasaereh@gmail.com>
2023-11-13 14:10:39 +0100

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const numArguments = process.argv.length - 2;
+if (numArguments === 0) {
+  console.log("No argument");
+} else if (numArguments === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
