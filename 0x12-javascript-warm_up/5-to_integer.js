aacb75f36de2ae568d36db4491ed35fff824b884
sanotogii <nasaereh@gmail.com>
2023-11-13 16:40:34 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-if (!isNaN(process.argv[2]) || process.argv[2] !== undefined) {
-  console.log('My number: ', int);
-} else {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  
   console.log('Not a number');
+} else {
+  console.log('My number: ', int);
 }
