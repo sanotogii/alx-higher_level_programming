abd83ec650fbec1b38ef0b40c12d1fa01277263d
sanotogii <nasaereh@gmail.com>
2023-11-14 19:20:18 +0100

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ class Square extends Ref {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      console.log('c'.repeat(this.width));
+      console.log(c.repeat(this.width));
     }
   }
 }
