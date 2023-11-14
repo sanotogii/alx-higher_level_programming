4bf7aac6a1ef4f04bba691b794a500b681c2e0ef
sanotogii <nasaereh@gmail.com>
2023-11-14 16:29:59 +0100

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (h <= 0 || w <= 0 || Number.isInteger(w) || Number.isInteger(h)) {
+    if (h <= 0 || w <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       return {};
-    }
+    } else {
     this.width = w;
     this.height = h;
+    }
   }
 }
 module.exports = Rectangle;
