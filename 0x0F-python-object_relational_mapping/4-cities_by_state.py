5ead2865af4f37be848fc4cddf7b30671e39cd24
sanotogii <nasaereh@gmail.com>
2023-11-18 18:08:02 +0000

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROMv \
+                cities INNER JOIN states ON states.id=cities.state_id")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
