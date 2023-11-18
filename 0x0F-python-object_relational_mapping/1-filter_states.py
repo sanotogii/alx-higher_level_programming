a1e568752e5d116025f374943f36a47f9873013e
sanotogii <nasaereh@gmail.com>
2023-11-18 17:15:44 +0000

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- script that lists all states with a name starting 
+ script that lists all states with a name starting
  with N (upper N) from the database hbtn_0e_0_usa
 """
 import MySQLdb
