b7a98e506ee8c5728a9e8ca7640e227987944d28
sanotogii <121005704+sanotogii@users.noreply.github.com>
2023-11-27 13:51:37 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-
-const data1 = fs.readFileSync(process.argv[2]).toString();
-const data2 = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], data1 + data2);
