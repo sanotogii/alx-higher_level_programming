8e582f402f8c39a5e7f70b65f5696d65c5a8d94a
sanotogii <nasaereh@gmail.com>
2024-03-12 02:36:06 +0000

    n-task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arg = Number(process.argv[2]);
-if (Number.isNaN(arg) || arg === undefined) {
+if (Number.isNaN(arg)) {
   console.log('Not a number');
 } else {
   console.log('My number: ', arg);
