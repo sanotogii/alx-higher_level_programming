c68d8b449ab0be764f595677ca080163ba3e2334
sanotogii <nasaereh@gmail.com>
2024-03-12 22:59:13 +0000

    6-task

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const seq = require('./5-square');
+
+class Square extends seq {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
