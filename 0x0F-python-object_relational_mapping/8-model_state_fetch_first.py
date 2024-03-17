21b357995fac500dc0ca683ec0af168f168868f8
sanotogii <nasaereh@gmail.com>
2024-03-17 01:40:01 +0000

    8-task

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     # Create engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'\
-                           .format(username, password, db_name),\
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
                            pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
