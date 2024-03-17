2cf3716d4a7c39bfa80fa3151c2d67f7167d15aa
sanotogii <nasaereh@gmail.com>
2024-03-17 02:00:56 +0000

    task

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -12,9 +12,8 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306\
+            /{}'.format(username, password, db_name), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
