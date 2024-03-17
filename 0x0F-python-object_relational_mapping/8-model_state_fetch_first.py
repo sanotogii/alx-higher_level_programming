2fe71a5b9ac7fcac39380fc6d0f836d713fd5f49
sanotogii <nasaereh@gmail.com>
2024-03-17 01:38:57 +0000

    8-task

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Module to list the first State object from the database hbtn_0e_6_usa """
+
+import sys
+from sqlalchmy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Create engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'\
+                           .format(username, password, db_name),\
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(first_state.id, first_state.name))
+
+    session.close()
