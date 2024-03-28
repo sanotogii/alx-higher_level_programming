384393c0f47bde8b8774c6227f2aac2306df3193
sanotogii <nasaereh@gmail.com>
2024-03-28 21:08:03 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL, and displays the size of the body of the response.
-
-url=$1
-curl -sI "$url" | grep 'Content-Length' | cut -d' ' -f2
+# This script takes a URL as an argument
+curl -sI "$1" | grep 'Content-Length' | cut -d' ' -f2
