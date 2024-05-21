d15b255aac79ef5548b6427921f74354f34d37ab
sanotogii <nasaereh@gmail.com>
2024-05-21 22:29:35 +0200

    execwrite content of a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const data = process.argv[3];
+
+if (!filePath) {
+  process.exit(1);
+}
+
+fs.writeFile(filePath, data, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
