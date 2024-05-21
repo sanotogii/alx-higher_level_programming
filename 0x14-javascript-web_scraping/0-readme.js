3d2ab4c1ea617fe8c26a54bb2fceb47de6853dc1
sanotogii <nasaereh@gmail.com>
2024-05-21 22:15:01 +0200

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
