2e14ed3a903f6270ead8f2c5fe21609e8c895737
sanotogii <nasaereh@gmail.com>
2024-05-23 02:41:36 +0200

    task 3 +2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const Id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/${Id}';
+const url = `https://swapi-api.alx-tools.com/api/films/${Id}`;
 
 if (!Id) {
   process.exit(1);
@@ -11,13 +11,8 @@ if (!Id) {
 request(url, (error, response, body) => {
   if (error) {
     console.log(error.code);
-  }
-
-  if (response.statusCode !== 200) {
-    console.log('code:', response.statusCode);
   } else {
     const json = JSON.parse(body);
     console.log(json.title);
   }
-}
-);
+});
