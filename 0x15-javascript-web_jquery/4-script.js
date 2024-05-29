a1793ca6eb08adc96fc37a513056e7c1baaea017
sanotogii <nasaereh@gmail.com>
2024-05-29 20:02:50 +0200

    task 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('DIV#toggle_header').on('click', function () {
+        $('header').toggleClass(function () {
+            if ($(this).hasClass('red')) {
+                return 'green';
+              } else {
+                return 'red';
+              }
+          });
+    });
+  });
